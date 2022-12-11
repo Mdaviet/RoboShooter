308a40a246b85bd12a757861c63503525b1ce411
Michael Daviet <Michael@thedaviets.com>
2022-12-11 13:22:22 -0700

    Add player with mouse rotation

---
+++ RoboShooter/Assets/Movement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate Player to mouse
+        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
+        transform.up = direction;
+    }
+}
