ccf0b387978ec2e685e7c9c46985c6e131fa4938
Michael Daviet <Michael@thedaviets.com>
2023-01-19 17:59:46 -0700

    Fix enemy spawning on edge

--- RoboShooter/Assets/Scripts/SpawnManager.cs
+++ RoboShooter/Assets/Scripts/SpawnManager.cs
@@ -27,20 +27,19 @@ public class SpawnManager : MonoBehaviour
 
     public void SpawnEnemy()
     {
-        /* Vector2 randomSpawn = new Vector2(Random.Range(-24, 24), Random.Range(-10, 10));
+        Vector2 randomSpawn = new Vector2(Random.Range(-12, 12), Random.Range(-5,5));
+        int randomSide = Random.Range(0,2);
 
-         if (randomSpawn.x > -12 && randomSpawn.x <= 0) randomSpawn.x -= 12;
-
-         else if (randomSpawn.x >= 0 && randomSpawn.x < 12) randomSpawn.x += 12;
-        */
-
-        Vector2 randomSpawn = new Vector2(Random.Range(-12, 12), Random.Range(-5, 5));
-
-        if (randomSpawn.x < 0) randomSpawn.x -= 12;
-        else randomSpawn.x += 12;
-
-        if (randomSpawn.y < 0) randomSpawn.y -= 5;
-        else randomSpawn.y += 5;
+        if (randomSide < 1)
+        {
+            if (randomSpawn.x <= 0) randomSpawn.x -= (12 + randomSpawn.x);
+            else randomSpawn.x += (12 - randomSpawn.x);
+        }
+        else
+        {
+            if (randomSpawn.y <= 0) randomSpawn.y -= (5 + randomSpawn.y);
+            else randomSpawn.y += (5 - randomSpawn.y);
+        }
 
         Instantiate(enemy, new Vector3(randomSpawn.x, randomSpawn.y, 0), Quaternion.identity);
     }
